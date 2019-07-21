744dcbd72e906b95ef847568055d69c05c8ebfcc
Lowell Smith <merickowa@gmail.com>
2019-07-21 14:24:22 -0400

    Simple image drawing

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using System.Drawing;
 
 namespace RayTracing
 {
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nx = 200;
+            var ny = 100;
+
+            var bmp = new Bitmap(nx, ny, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+            for (int j = 0; j < ny ; j++)
+            {
+                for (int i = 0; i < nx; i++)
+                {
+                    var r = (float)i / nx;
+                    var g = (float)j / ny;
+                    var b = 0.2f;
+
+                    var ir = (int)(255.99*r);
+                    var ig = (int)(255.99*g);
+                    var ib = (int)(255.99*b);
+
+                    bmp.SetPixel(i, j, Color.FromArgb(ir, ig, ib));
+                }
+            }
+
+            bmp.Save("ouput.png");
         }
     }
 }
